ad0350ad102663c9e70798fb868fd41688746ec2
Akash Khan <akashkhan25@gmail.com>
2016-06-05 18:10:19 +0530

    Added dependencies and api models

---
+++ app/src/main/java/in/ac/iitb/gymkhana/testapp/GsonModels.java
@@ -0,0 +1,81 @@
+package in.ac.iitb.gymkhana.testapp;
+
+import java.util.List;
+
+/**
+ * Created by akash on 5/6/16.
+ */
+
+public class GsonModels {
+    public class User{
+        private String login;
+        private int id;
+        private String avatarUrl;
+
+        public User(String login, int id, String avatarUrl) {
+            this.login = login;
+            this.id = id;
+            this.avatarUrl = avatarUrl;
+        }
+
+        public String getLogin() {
+            return login;
+        }
+
+        public void setLogin(String login) {
+            this.login = login;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getAvatarUrl() {
+            return avatarUrl;
+        }
+
+        public void setAvatarUrl(String avatarUrl) {
+            this.avatarUrl = avatarUrl;
+        }
+    }
+
+    public class SearchResult{
+        private int totalCount;
+        private boolean incompleteResults;
+        private List<User> items;
+
+        public SearchResult(int totalCount, boolean incompleteResults, List<User> items) {
+            this.totalCount = totalCount;
+            this.incompleteResults = incompleteResults;
+            this.items = items;
+        }
+
+        public int getTotalCount() {
+            return totalCount;
+        }
+
+        public void setTotalCount(int totalCount) {
+            this.totalCount = totalCount;
+        }
+
+        public boolean isIncompleteResults() {
+            return incompleteResults;
+        }
+
+        public void setIncompleteResults(boolean incompleteResults) {
+            this.incompleteResults = incompleteResults;
+        }
+
+        public List<User> getItems() {
+            return items;
+        }
+
+        public void setItems(List<User> items) {
+            this.items = items;
+        }
+    }
+}
