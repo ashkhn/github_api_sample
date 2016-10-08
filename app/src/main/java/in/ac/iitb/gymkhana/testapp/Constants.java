d5f443890867f06a112f780dade253cfc51ba9bc
Sajal Narang <sajalnarang@gmail.com>
2016-10-08 18:22:49 +0530

    Updated UserDetailsActivity

--- app/src/main/java/in/ac/iitb/gymkhana/testapp/Constants.java
+++ app/src/main/java/in/ac/iitb/gymkhana/testapp/Constants.java
@@ -8,4 +8,5 @@ public class Constants {
     public static final String KEY_SEARCH_QUERY = "search_query";
     public static final String KEY_QUERY_BUNDLE = "query_bundle";
     public static final String KEY_USER_LOGIN = "user_login";
+    public static final String KEY_USER_AVATAR = "user_avatar";
 }
--- app/src/main/java/in/ac/iitb/gymkhana/testapp/UserDetailActivity.java
+++ app/src/main/java/in/ac/iitb/gymkhana/testapp/UserDetailActivity.java
@@ -3,8 +3,15 @@ package in.ac.iitb.gymkhana.testapp;
 import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -14,11 +21,17 @@ public class UserDetailActivity extends AppCompatActivity
         implements Callback<GsonModels.UserDetails>{
 
     private String userLogin;
+    private String userAvatarUrl;
 
     private ProgressDialog progressDialog;
+    private ImageView userProfilePic;
     private TextView userNameTv;
+    private TextView userHandleTv;
     private TextView userBioTv;
-    private TextView userFollowersTv;
+    private TextView userLocationTv;
+    private TextView userEmailTv;
+
+    private LinearLayout detailsLl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +39,16 @@ public class UserDetailActivity extends AppCompatActivity
         setContentView(R.layout.activity_user_detail);
         if(getIntent()!=null){
             userLogin = getIntent().getStringExtra(Constants.KEY_USER_LOGIN);
+            userAvatarUrl = getIntent().getStringExtra(Constants.KEY_USER_AVATAR);
         }
 
+        userProfilePic = (ImageView) findViewById(R.id.user_profile_pic);
         userNameTv = (TextView) findViewById(R.id.user_name);
+        userHandleTv = (TextView) findViewById(R.id.user_handle);
         userBioTv = (TextView) findViewById(R.id.user_bio);
-        userFollowersTv = (TextView) findViewById(R.id.user_followers);
+        userLocationTv = (TextView) findViewById(R.id.user_location);
+        userEmailTv = (TextView) findViewById(R.id.user_email);
+        detailsLl = (LinearLayout) findViewById(R.id.details);
 
 
         progressDialog = new ProgressDialog(UserDetailActivity.this);
@@ -46,9 +64,13 @@ public class UserDetailActivity extends AppCompatActivity
     public void onResponse(Call<GsonModels.UserDetails> call, Response<GsonModels.UserDetails> response) {
         if(response.isSuccessful()){
             GsonModels.UserDetails userDetails = response.body();
+            Picasso.with(this).load(userAvatarUrl).into(userProfilePic);
             userNameTv.setText(userDetails.getName());
+            userHandleTv.setText(userLogin);
             userBioTv.setText(userDetails.getBio());
-            userFollowersTv.setText(String.valueOf(userDetails.getFollowers()));
+            userLocationTv.setText(userDetails.getLocation());
+            userEmailTv.setText(userDetails.getEmail());
+            detailsLl.setVisibility(View.VISIBLE);
         }
         else {
             Toast.makeText(UserDetailActivity.this, "Response code" + response.code(), Toast.LENGTH_SHORT).show();
